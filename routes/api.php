b932bc3df0156f9d1fa7f02becd72cb036cab5bd
Jalynn <jalynnking@gmail.com>
2022-08-04 16:29:36 -0400

    Create photo controller and establish routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PhotographerController;
 use App\Http\Controllers\API\AlbumController;
+use App\Http\Controllers\API\PhotoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('photographers', PhotographerController::class);
 
 Route::apiResource('albums', AlbumController::class);
+
+Route::apiResource('photos', PhotoController::class);
