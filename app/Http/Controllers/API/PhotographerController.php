a5ea4fc1ca0ebd6316d23d2d7e3de2c3099a0efc
Jalynn <jalynnking@gmail.com>
2022-08-04 02:10:31 -0400

    Create photographer controller and instantiate photographer controller route -- testing done in postman

---
+++ app/Http/Controllers/API/PhotographerController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Photographer;
+
+class PhotographerController extends Controller
+{
+    // Get all photographers
+    public function index()
+    {
+        $photographers = Photographer::all();
+        return response()->json($photographers);
+    }
+
+    // Create a new photographer
+    public function store(Request $request)
+    {
+        // Validate request
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'bio' => 'required'
+        ]);
+        // Try to create new photographer
+        try {
+            // Get new photographer data from request
+            $newPhotographer = new Photographer([
+                'name' => $request->get('name'),
+                'phone' => $request->get('phone'),
+                'email' => $request->get('email'),
+                'bio' => $request->get('bio')
+            ]);
+            // Save new photographer
+            $newPhotographer->save();
+            // Return nrew photographer
+            return response()->json($newPhotographer);
+        // Error handling if create new photographer fails
+        } catch(\Exception $error) {
+            \Log::error($error->getMessage());
+                return response()->json([
+                    'message' => 'There was an error with creating your photographer.'
+                ], 500);
+        };
+    }
+
+    // Get photographer by id
+    public function show($id)
+    {
+        $photographer = Photographer::findOrFail($id);
+        return response()->json($photographer);
+    }
+
+    // Update specific photographer
+    public function update(Request $request, $id)
+    {
+        // Validate request
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'bio' => 'required'
+        ]);
+        // Get photographer to update
+        $photographer = Photographer::findOrFail($id);
+        // Make updates with data from request
+        $photographer->name = $request->get('name');
+        $photographer->phone = $request->get('phone');
+        $photographer->email = $request->get('email');
+        $photographer->bio = $request->get('bio');
+        // Save changes
+        $photographer->save();
+        // Return updated photographer
+        return response()->json($photographer);
+    }
+
+    // Delete specific photographer
+    public function destroy($id)
+    {
+        // Find photographer to be deleted
+        $photographer = Photographer::findOrFail($id);
+        // Delete photographer
+        $photographer->delete();
+        // Return updated list of photographers
+        return response()->json($photographer::all());
+    }
+}
